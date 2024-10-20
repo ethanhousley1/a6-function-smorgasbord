484377134bf287bb508e29655feb10614db15e75
ethanhousley1 <ethanhousley@gmail.com>
2024-10-19 22:35:34 -0600

    finished!

--- a6_function_smorgasbord.py
+++ a6_function_smorgasbord.py
@@ -1,2 +1,42 @@
-# Name:
+# Name: Ethan Housley
 # Enter your python code below. Replace this line with a description.
+# lots of functions will be called to this file. we must import them
+from a6_my_functions import *
+
+
+# this is the welcome message
+
+def welcome_message(name):
+    print(f'Hello {name}, welcome to IS 303!')
+
+welcome_message('Diego')
+welcome_message('Mai')
+
+
+print(sum_two_numbers(5,7))
+print(sum_two_numbers(10000.5, -30))
+
+print(is_even(7))
+print(is_even(120))
+
+print(get_number_parity(5))
+print(get_number_parity(10))
+
+print(fahrenheit_to_celsius(32))
+print(fahrenheit_to_celsius(75))
+
+numbers_list_1 = [20, 45, 23, 2, 87, 3]
+print(min_max_mean(numbers_list_1))
+
+print(dog_message('Spot', 7))
+print(dog_message('Peppy'))
+
+print(classify_age(60, 55))
+print(classify_age(62))
+
+for i in range(2):
+    price = input('Enter the price for the product purchased: ')
+    quantity = input('Enter the quantity of the product purchased: ')
+    discount_percent = input('Enter the discount percent (formatted as a decimal): ')
+    total = calculate_total(price, quantity, discount_percent)
+    print(f"The total price after discounts is: ${total}")
--- a6_my_functions.py
+++ a6_my_functions.py
@@ -1,2 +1,57 @@
-# Name:
+# Name: Ethan Housley
 # You will place most of your functions for this assignment in this file.
+
+# this is the sum of 2 numbers, which is called in function smorgasbord
+def sum_two_numbers(num1, num2):
+    return num1 + num2
+
+# is even function
+def is_even(int):
+    return int % 2 == 0
+
+# this function will tell you if a int is even or odd
+def get_number_parity(int):
+    if is_even(int):
+        return f'{int} is even'
+    else:
+        return f'{int} is odd'
+
+def fahrenheit_to_celsius(temp):
+    celsius = (temp - 32) * (5/9)
+    return celsius
+
+def min_max_mean(lst):
+    min = lst[0]
+    max = lst[0]
+    mean = 0
+    for i in lst:
+        # this will go thru and change the min to the new lowest num
+        if min > i:
+            min = i
+        # this will go thru and change the min to the new lowest num
+        if max < i:
+            max = i
+        mean += i
+    mean = mean / len(lst)
+    return [min, max, mean]
+
+def dog_message(name, age=0):
+    return f"I am a dog named {name} and I'm {age} years old!"
+
+def classify_age(age, senior_age=65):
+    if age < 18:
+        return 'Minor'
+    elif age < senior_age:
+        return 'Adult'
+    else:
+        return 'Senior'
+
+def calculate_total(price, quantity, discount_percent, threshold_total=100, bonus_discount=float(0.02)):
+    subtotal = float(price) * float(quantity)
+    if subtotal <= threshold_total:
+        total = subtotal - subtotal * float(discount_percent)
+    # this elif statement isn't necessary but i realized it after the fact
+    elif subtotal > threshold_total:
+        # honestly not sure if I need to make sure discount percent and bonus discount are floats, did it anyway
+        total = subtotal - subtotal * (float(discount_percent) + float(bonus_discount))
+    return total
